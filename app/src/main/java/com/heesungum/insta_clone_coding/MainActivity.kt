a1d4d2ed2d44b468b8eaa4f5dac5776fa889d4c4
HeeSungUm <50452867+HeeSungUm@users.noreply.github.com>
2021-05-19 20:36:16 +0900

    메인 액티비티 아이콘 추가

--- app/src/main/java/com/heesungum/insta_clone_coding/MainActivity.kt
+++ app/src/main/java/com/heesungum/insta_clone_coding/MainActivity.kt
@@ -2,6 +2,7 @@ package com.heesungum.insta_clone_coding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
@@ -13,10 +14,42 @@ class MainActivity : AppCompatActivity() {
 
         val viewPager2 : ViewPager2 = findViewById(R.id.pager)
         val tabLayout : TabLayout = findViewById(R.id.tab_layout)
+        val recyclerView : RecyclerView = findViewById(R.id.timeline_rv)
+
+
 
         viewPager2.adapter = PagerAdapter(this)
         TabLayoutMediator(tabLayout, viewPager2) {tab, position ->
-            tab.text = "OBJECT ${(position+1)}"
+            when (position) {
+                0 -> tab.setIcon(R.drawable.ic_outline_home_24)
+                1 -> tab.setIcon(R.drawable.ic_baseline_search_24)
+                2 -> tab.setIcon(R.drawable.ic_baseline_ondemand_video_24)
+                3 -> tab.setIcon(R.drawable.ic_outline_shopping_bag_24)
+                4 -> tab.setIcon(R.drawable.ic_outline_person_24)
+            }
         }.attach()
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab?.position) {
+                    0 -> tab.setIcon(R.drawable.ic_baseline_home_24)
+                    3 -> tab.setIcon(R.drawable.ic_baseline_shopping_bag_24)
+                    4 -> tab.setIcon(R.drawable.ic_baseline_person_24)
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                when (tab?.position) {
+                    0 -> tab.setIcon(R.drawable.ic_outline_home_24)
+                    1 -> tab.setIcon(R.drawable.ic_baseline_search_24)
+                    2 -> tab.setIcon(R.drawable.ic_baseline_ondemand_video_24)
+                    3 -> tab.setIcon(R.drawable.ic_outline_shopping_bag_24)
+                    4 -> tab.setIcon(R.drawable.ic_outline_person_24)
+                }
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+        })
     }
 }
