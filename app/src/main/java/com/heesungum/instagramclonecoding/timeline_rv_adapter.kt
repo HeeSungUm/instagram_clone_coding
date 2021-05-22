297bac5589b2d23b2facd817adae044a062d1252
HeeSungUm <50452867+HeeSungUm@users.noreply.github.com>
2021-05-22 13:39:23 +0900

    타임라인 콘텐츠 이미지뷰 추가

--- app/src/main/java/com/heesungum/instagramclonecoding/timeline_rv_adapter.kt
+++ app/src/main/java/com/heesungum/instagramclonecoding/timeline_rv_adapter.kt
@@ -3,6 +3,7 @@ package com.heesungum.instagramclonecoding
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import de.hdodenhof.circleimageview.CircleImageView
@@ -10,7 +11,8 @@ import de.hdodenhof.circleimageview.CircleImageView
 class timeline_rv_adapter(_itemCount:Int) : RecyclerView.Adapter<timeline_rv_adapter.ViewHolder>() {
     private val itemCount = _itemCount
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val imageView : CircleImageView = itemView.findViewById(R.id.profile_civ)
+        val profileImageView : CircleImageView = itemView.findViewById(R.id.profile_civ)
+        val mainContentImageView : ImageView = itemView.findViewById(R.id.timeline_main_iv)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -19,7 +21,8 @@ class timeline_rv_adapter(_itemCount:Int) : RecyclerView.Adapter<timeline_rv_ada
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.imageView.load("https://cdn.womaneconomy.kr/news/photo/202008/93227_303386_4457.jpg")
+        holder.profileImageView.load("https://cdn.womaneconomy.kr/news/photo/202008/93227_303386_4457.jpg")
+        holder.mainContentImageView.load("https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=http%3A%2F%2Fcfile28.uf.tistory.com%2Fimage%2F99149F3359B87F0520EE68")
     }
 
     override fun getItemCount(): Int = itemCount
